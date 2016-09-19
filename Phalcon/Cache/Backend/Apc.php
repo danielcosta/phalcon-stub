214c727c16b84fea38e61c98568301d8afa5fa4c
Daniel Costa <maz_dcosta@uolinc.com>
2016-09-19 09:57:26 -0300

    Adding stubs for version 3.0.0

---
+++ Phalcon/Cache/Backend/Apc.php
@@ -0,0 +1,101 @@
+<?php 
+
+namespace Phalcon\Cache\Backend {
+
+	/**
+	 * Phalcon\Cache\Backend\Apc
+	 *
+	 * Allows to cache output fragments, PHP data and raw data using an APC backend
+	 *
+	 *<code>
+	 * use Phalcon\Cache\Backend\Apc;
+	 * use Phalcon\Cache\Frontend\Data as FrontData;
+	 *
+	 * // Cache data for 2 days
+	 * $frontCache = new FrontData([
+	 *     'lifetime' => 172800
+	 * ]);
+	 *
+	 * $cache = new Apc($frontCache, [
+	 *     'prefix' => 'app-data'
+	 * ]);
+	 *
+	 * // Cache arbitrary data
+	 * $cache->save('my-data', [1, 2, 3, 4, 5]);
+	 *
+	 * // Get data
+	 * $data = $cache->get('my-data');
+	 *</code>
+	 */
+	
+	class Apc extends \Phalcon\Cache\Backend implements \Phalcon\Cache\BackendInterface {
+
+		/**
+		 * Returns a cached content
+		 */
+		public function get($keyName, $lifetime=null){ }
+
+
+		/**
+		 * Stores cached content into the APC backend and stops the frontend
+		 *
+		 * @param string|long keyName
+		 * @param string content
+		 * @param long lifetime
+		 * @param boolean stopBuffer
+		 */
+		public function save($keyName=null, $content=null, $lifetime=null, $stopBuffer=null){ }
+
+
+		/**
+		 * Increment of a given key, by number $value
+		 *
+		 * @param  string keyName
+		 * @param  long value
+		 * @return mixed
+		 */
+		public function increment($keyName=null, $value=null){ }
+
+
+		/**
+		 * Decrement of a given key, by number $value
+		 *
+		 * @param  string keyName
+		 * @param  long value
+		 * @return mixed
+		 */
+		public function decrement($keyName=null, $value=null){ }
+
+
+		/**
+		 * Deletes a value from the cache by its key
+		 */
+		public function delete($keyName){ }
+
+
+		/**
+		 * Query the existing cached keys
+		 *
+		 * @param string prefix
+		 * @return array
+		 */
+		public function queryKeys($prefix=null){ }
+
+
+		/**
+		 * Checks if cache exists and it hasn't expired
+		 *
+		 * @param  string|long keyName
+		 * @param  long lifetime
+		 * @return boolean
+		 */
+		public function exists($keyName=null, $lifetime=null){ }
+
+
+		/**
+		 * Immediately invalidates all existing items.
+		 */
+		public function flush(){ }
+
+	}
+}
---
+++ Phalcon/Dispatcher.php
@@ -0,0 +1,287 @@
+<?php 
+
+namespace Phalcon {
+
+	/**
+	 * Phalcon\Dispatcher
+	 *
+	 * This is the base class for Phalcon\Mvc\Dispatcher and Phalcon\Cli\Dispatcher.
+	 * This class can't be instantiated directly, you can use it to create your own dispatchers.
+	 */
+	
+	abstract class Dispatcher implements \Phalcon\DispatcherInterface, \Phalcon\Di\InjectionAwareInterface, \Phalcon\Events\EventsAwareInterface {
+
+		const EXCEPTION_NO_DI = 0;
+
+		const EXCEPTION_CYCLIC_ROUTING = 1;
+
+		const EXCEPTION_HANDLER_NOT_FOUND = 2;
+
+		const EXCEPTION_INVALID_HANDLER = 3;
+
+		const EXCEPTION_INVALID_PARAMS = 4;
+
+		const EXCEPTION_ACTION_NOT_FOUND = 5;
+
+		protected $_dependencyInjector;
+
+		protected $_eventsManager;
+
+		protected $_activeHandler;
+
+		protected $_finished;
+
+		protected $_forwarded;
+
+		protected $_moduleName;
+
+		protected $_namespaceName;
+
+		protected $_handlerName;
+
+		protected $_actionName;
+
+		protected $_params;
+
+		protected $_returnedValue;
+
+		protected $_lastHandler;
+
+		protected $_defaultNamespace;
+
+		protected $_defaultHandler;
+
+		protected $_defaultAction;
+
+		protected $_handlerSuffix;
+
+		protected $_actionSuffix;
+
+		protected $_previousNamespaceName;
+
+		protected $_previousHandlerName;
+
+		protected $_previousActionName;
+
+		protected $_modelBinding;
+
+		/**
+		 * Sets the dependency injector
+		 */
+		public function setDI(\Phalcon\DiInterface $dependencyInjector){ }
+
+
+		/**
+		 * Returns the internal dependency injector
+		 */
+		public function getDI(){ }
+
+
+		/**
+		 * Sets the events manager
+		 */
+		public function setEventsManager(\Phalcon\Events\ManagerInterface $eventsManager){ }
+
+
+		/**
+		 * Returns the internal event manager
+		 */
+		public function getEventsManager(){ }
+
+
+		/**
+		 * Sets the default action suffix
+		 */
+		public function setActionSuffix($actionSuffix){ }
+
+
+		/**
+		 * Gets the default action suffix
+		 */
+		public function getActionSuffix(){ }
+
+
+		/**
+		 * Sets the module where the controller is (only informative)
+		 */
+		public function setModuleName($moduleName){ }
+
+
+		/**
+		 * Gets the module where the controller class is
+		 */
+		public function getModuleName(){ }
+
+
+		/**
+		 * Sets the namespace where the controller class is
+		 */
+		public function setNamespaceName($namespaceName){ }
+
+
+		/**
+		 * Gets a namespace to be prepended to the current handler name
+		 */
+		public function getNamespaceName(){ }
+
+
+		/**
+		 * Sets the default namespace
+		 */
+		public function setDefaultNamespace($namespaceName){ }
+
+
+		/**
+		 * Returns the default namespace
+		 */
+		public function getDefaultNamespace(){ }
+
+
+		/**
+		 * Sets the default action name
+		 */
+		public function setDefaultAction($actionName){ }
+
+
+		/**
+		 * Sets the action name to be dispatched
+		 */
+		public function setActionName($actionName){ }
+
+
+		/**
+		 * Gets the latest dispatched action name
+		 */
+		public function getActionName(){ }
+
+
+		/**
+		 * Sets action params to be dispatched
+		 *
+		 * @param array params
+		 */
+		public function setParams($params){ }
+
+
+		/**
+		 * Gets action params
+		 */
+		public function getParams(){ }
+
+
+		/**
+		 * Set a param by its name or numeric index
+		 *
+		 * @param  mixed param
+		 * @param  mixed value
+		 */
+		public function setParam($param, $value){ }
+
+
+		/**
+		 * Gets a param by its name or numeric index
+		 *
+		 * @param  mixed param
+		 * @param  string|array filters
+		 * @param  mixed defaultValue
+		 * @return mixed
+		 */
+		public function getParam($param, $filters=null, $defaultValue=null){ }
+
+
+		/**
+		 * Check if a param exists
+		 *
+		 * @param  mixed param
+		 * @return boolean
+		 */
+		public function hasParam($param){ }
+
+
+		/**
+		 * Returns the current method to be/executed in the dispatcher
+		 */
+		public function getActiveMethod(){ }
+
+
+		/**
+		 * Checks if the dispatch loop is finished or has more pendent controllers/tasks to dispatch
+		 */
+		public function isFinished(){ }
+
+
+		/**
+		 * Sets the latest returned value by an action manually
+		 *
+		 * @param mixed value
+		 */
+		public function setReturnedValue($value){ }
+
+
+		/**
+		 * Returns value returned by the latest dispatched action
+		 *
+		 * @return mixed
+		 */
+		public function getReturnedValue(){ }
+
+
+		/**
+		 * Enable/Disable model binding during dispatch
+		 *
+		 * @param boolean value
+		 */
+		public function setModelBinding($value){ }
+
+
+		/**
+		 * Dispatches a handle action taking into account the routing parameters
+		 *
+		 * @return object
+		 */
+		public function dispatch(){ }
+
+
+		/**
+		 * Dispatches a handle action taking into account the routing parameters
+		 *
+		 * @return object
+		 */
+		protected function _dispatch(){ }
+
+
+		/**
+		 * Forwards the execution flow to another controller/action
+		 * Dispatchers are unique per module. Forwarding between modules is not allowed
+		 *
+		 *<code>
+		 *  $this->dispatcher->forward(array("controller" => "posts", "action" => "index"));
+		 *</code>
+		 *
+		 * @param array forward
+		 */
+		public function forward($forward){ }
+
+
+		/**
+		 * Check if the current executed action was forwarded by another one
+		 */
+		public function wasForwarded(){ }
+
+
+		/**
+		 * Possible class name that will be located to dispatch the request
+		 */
+		public function getHandlerClass(){ }
+
+
+		public function callActionMethod($handler, $actionMethod, $params=null){ }
+
+
+		/**
+		 * Set empty properties to their defaults (where defaults are available)
+		 */
+		protected function _resolveEmptyProperties(){ }
+
+	}
+}
