ce6ca051b70a82142d2aee16836ec4bab1814373
Daniel Costa <danielcosta@gmail.com>
2016-09-19 09:57:26 -0300

    Adding stubs for version 3.0.0

---
+++ Phalcon/Validation/Validator/CreditCard.php
@@ -0,0 +1,42 @@
+<?php 
+
+namespace Phalcon\Validation\Validator {
+
+	/**
+	 * Phalcon\Validation\Validator\CreditCard
+	 *
+	 * Checks if a value has a valid credit card number
+	 *
+	 * <code>
+	 * use Phalcon\Validation\Validator\CreditCard as CreditCardValidator;
+	 *
+	 * $validator->add('creditcard', new CreditCardValidator([
+	 *     'message' => 'The credit card number is not valid'
+	 * ]));
+	 *
+	 * $validator->add(['creditcard', 'secondCreditCard'], new CreditCardValidator([
+	 *     'message' => [
+	 *         'creditcard' => 'The credit card number is not valid',
+	 *         'secondCreditCard' => 'The second credit card number is not valid'
+	 *     ]
+	 * ]));
+	 * </code>
+	 */
+	
+	class CreditCard extends \Phalcon\Validation\Validator implements \Phalcon\Validation\ValidatorInterface {
+
+		/**
+		 * Executes the validation
+		 */
+		public function validate(\Phalcon\Validation $validation, $field){ }
+
+
+		/**
+		 * is a simple checksum formula used to validate a variety of identification numbers
+		 * @param  string number
+		 * @return boolean
+		 */
+		private function verifyByLuhnAlgorithm($number){ }
+
+	}
+}
